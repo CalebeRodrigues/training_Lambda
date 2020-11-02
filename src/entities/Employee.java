95a6e59fb38510ee31979349524c859e049de08e
Calebe Rodrigues <calebebrodrigues@gmail.com>
2020-11-02 20:51:18 -0300

    Initial commit

---
+++ src/entities/Employee.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class Employee {
+	private String name;
+	private String email;
+	private Double salary;
+	
+	public String getName() {
+		return name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+		
+}
